b6299937794321006e447f3f01bf1df75c11c226
KarthickTechsyntax <karthicktechsyntax@gmail.com>
2018-07-03 18:24:36 +0530

    Added country list from first user

--- myphp.php
+++ myphp.php
@@ -1,3 +1,13 @@
-<?php echo 'My php Code here';
-echo 'Im OM Heyyy';
+<?php 
+$travel = 'home country';
+if($travel == 'Home Country')
+{
+	echo 'Madurai';
+	echo 'Bangalore';	
+}
+else
+{
+	echo 'Singapore';
+}
+
  ?>
